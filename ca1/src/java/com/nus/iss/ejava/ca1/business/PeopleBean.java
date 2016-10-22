4edea9a8db95e7040a7fe480ca73cd9f11cf33f6
Mayuran Satchi <mayuran19@gmail.com>
2016-10-22 14:57:03 +0800

    ejb

---
+++ ca1/src/java/com/nus/iss/ejava/ca1/business/PeopleBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nus.iss.ejava.ca1.business;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author mayuran
+ */
+@Stateless
+public class PeopleBean {
+    @Resource(lookup = "jdbc/appointment")
+    private DataSource dataSource;
+    
+}
