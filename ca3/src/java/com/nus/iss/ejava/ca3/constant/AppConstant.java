e707e6404507a5fa668920a39a131483d65f7247
muguatnus <e0015144@u.nus.edu>
2016-11-12 10:33:58 +0800

    AppConstant

---
+++ ca3/src/java/com/nus/iss/ejava/ca3/constant/AppConstant.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nus.iss.ejava.ca3.constant;
+
+/**
+ *
+ * @author Mugunthan
+ */
+public class AppConstant {
+    public static final long HQ_ACK_WAIT_PERIOD = 1000;
+    public static final String HQ_POD_UPLOAD_URL = "http://10.10.0.48:8080/epod/upload";
+}
