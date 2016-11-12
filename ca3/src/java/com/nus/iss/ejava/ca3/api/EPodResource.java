5b4c3105507f8d02bbb144d993fd0f30d1076038
muguatnus <e0015144@u.nus.edu>
2016-11-12 11:09:28 +0800

    Resolved

--- ca3/src/java/com/nus/iss/ejava/ca3/api/EPodResource.java
+++ ca3/src/java/com/nus/iss/ejava/ca3/api/EPodResource.java
@@ -5,11 +5,10 @@
  */
 package com.nus.iss.ejava.ca3.api;
 
-<<<<<<< HEAD
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
-=======
 import com.nus.iss.ejava.ca3.business.DeliveryBusiness;
 import com.nus.iss.ejava.ca3.entity.Delivery;
 import java.util.List;
