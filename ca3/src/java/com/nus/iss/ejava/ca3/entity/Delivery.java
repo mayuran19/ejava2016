5b0e190e7e1fd78b26bdcfe061d1051399fc834f
Mayuran Satchi <mayuran19@gmail.com>
2016-11-12 10:09:25 +0800

    entiry

--- ca3/src/java/com/nus/iss/ejava/ca3/entity/Delivery.java
+++ ca3/src/java/com/nus/iss/ejava/ca3/entity/Delivery.java
@@ -6,27 +6,72 @@
 package com.nus.iss.ejava.ca3.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
  * @author mayuran
  */
 @Entity
+@Table(name = "delivery")
 public class Delivery implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column(name = "pkg_id")
+    private Integer pkgId;
+    private String name;
+    private String address;
+    private String phone;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date")
+    private Date createDate;
 
-    public Long getId() {
-        return id;
+    public Integer getPkgId() {
+        return pkgId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setPkgId(Integer pkgId) {
+        this.pkgId = pkgId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
     }
 }
---
+++ ca3/src/java/com/nus/iss/ejava/ca3/entity/Pod.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nus.iss.ejava.ca3.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author mayuran
+ */
+@Entity
+@Table(name = "pod")
+public class Pod implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "pod_id")
+    private Integer podId;
+    @OneToOne(fetch = FetchType.EAGER)
+    private Delivery delivery;
+    private String note;
+    @Lob
+    private byte[] image;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "delivery_date")
+    private Date deliveryDate;
+    @Column(name = "ack_id")
+    private String ackId;
+
+    public Integer getPodId() {
+        return podId;
+    }
+
+    public void setPodId(Integer podId) {
+        this.podId = podId;
+    }
+
+    public Delivery getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getAckId() {
+        return ackId;
+    }
+
+    public void setAckId(String ackId) {
+        this.ackId = ackId;
+    }
+}
